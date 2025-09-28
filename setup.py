b21606b832075dd780dffdeff8c13e5f2152e822
Mario Limonciello <superm1@kernel.org>
2025-09-27 21:44:18 -0500

    Loosen the python version requirement (#92)
    I have no problem running on Python 3.13.7
    
    Closes: https://github.com/amd/gaia/issues/91
    
    Signed-off-by: Mario Limonciello (AMD) <superm1@kernel.org>

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ setup(
             "gaia-mcp-atlassian = gaia.mcp.atlassian_mcp:main",
         ]
     },
-    python_requires=">=3.8, <3.13",
+    python_requires=">=3.8",
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     include_package_data=True,
